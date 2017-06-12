85a19e82b96fb0d896363de0e52e738196901e02
Matt Qunell <mqunell@gmail.com>
2017-06-12 16:00:36 -0700

    Ch 2: Implement cycling through questions via "Next" button

--- app/src/main/java/com/mattqunell/bignerdranch/QuizActivity.java
+++ app/src/main/java/com/mattqunell/bignerdranch/QuizActivity.java
@@ -5,17 +5,34 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextview;
+
+    private Question[] mQuestions = new Question[] {
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_oceans, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextview = (TextView) findViewById(R.id.question_textview);
+        updateQuestion();
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -36,5 +53,21 @@ public class QuizActivity extends AppCompatActivity {
                                Toast.LENGTH_SHORT).show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestions.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int questionId = mQuestions[mCurrentIndex].getTextResId();
+        mQuestionTextview.setText(questionId);
     }
 }
+
+// todo: Add true/false validation (currently true->correct, false->incorrect for all questions)
