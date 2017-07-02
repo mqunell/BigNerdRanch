43ce29e4b28db9d48b243a72d345b9e4e3e3b966
Matt Qunell <mqunell@gmail.com>
2017-07-01 23:07:31 -0700

    Ch 5: Create CheatActivity with initial implementation

--- app/src/main/java/com/mattqunell/bignerdranch/QuizActivity.java
+++ app/src/main/java/com/mattqunell/bignerdranch/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.mattqunell.bignerdranch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +18,7 @@ public class QuizActivity extends AppCompatActivity {
     private TextView mQuestionTextview;
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mCheatButton;
     private ImageButton mPreviousButton;
     private ImageButton mNextButton;
 
@@ -66,6 +68,15 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(false);
+            }
+        });
+
+        // Button: Cheat
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuizActivity.this, CheatActivity.class));
             }
         });
 
