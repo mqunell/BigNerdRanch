e6db55fd8ba6df4af7f882ea50f5dc8d1eac03e2
Matt Qunell <mqunell@gmail.com>
2017-07-14 19:27:58 -0700

    Ch 6: Test build versions/compatibilities with an animation

--- app/src/main/java/com/mattqunell/bignerdranch/CheatActivity.java
+++ app/src/main/java/com/mattqunell/bignerdranch/CheatActivity.java
@@ -1,11 +1,15 @@
 package com.mattqunell.bignerdranch;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -78,6 +82,27 @@ public class CheatActivity extends AppCompatActivity {
         }
 
         setAnswerShownResult(true);
+
+        // If the device's version is >= Lollipop
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            // Animation that makes mShowAnswerButton invisible
+            int cx = mShowAnswerButton.getWidth() / 2;
+            int cy = mShowAnswerButton.getHeight() / 2;
+            float radius = mShowAnswerButton.getWidth();
+            Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
+            });
+            anim.start();
+        }
+        else {
+            mShowAnswerButton.setVisibility(View.INVISIBLE);
+        }
     }
 
     // Send data back to the parent activity
