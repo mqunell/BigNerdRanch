1cecb02a087244bb4e09d3ee3d59623e74dd78b4
Matt Qunell <mqunell@gmail.com>
2017-07-18 20:25:50 -0700

    Set up MainActivity as the default/launchpad; add activity labels

--- app/src/main/java/com/mattqunell/bignerdranch/MainActivity.java
+++ app/src/main/java/com/mattqunell/bignerdranch/MainActivity.java
@@ -1,7 +1,12 @@
 package com.mattqunell.bignerdranch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mattqunell.bignerdranch.GeoQuiz.QuizActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button geoQuiz = (Button) findViewById(R.id.main_button_geoquiz);
+        geoQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
+            }
+        });
+
+        Button criminalIntent = (Button) findViewById(R.id.main_button_criminalintent);
+        criminalIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start CriminalIntent's main activity here
+            }
+        });
     }
 }
